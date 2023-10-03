d44cdc6e8a84c7c7c613cc6ba4ba6d2edafaa5d9
Afonso <dias4018@gmail.com>
2023-10-03 11:08:16 +0100

    adicionados ficheiros

--- write_noncanonical.c
+++ write_noncanonical.c
@@ -94,76 +94,68 @@ int sendFrame(unsigned char buf[],int n){
     }
 }
 
-int main(int argc, char *argv[])
-{
-    int counter = 0;
-    // Set alarm function handler
-    (void)signal(SIGALRM, alarmHandler);
- 
-        // Program usage: Uses either COM1 or COM2
-        const char *serialPortName = argv[1];
+int main(int argc, char *argv[]){
+	int counter = 0;
+	// Set alarm function handler
+	(void) signal(SIGALRM, alarmHandler);
 
-        if (argc < 2)
-        {
-            printf("Incorrect program usage\n"
-                   "Usage: %s <SerialPort>\n"
-                   "Example: %s /dev/ttyS1\n",
-                   argv[0],
-                   argv[0]);
-            exit(1);
-        }
+	// Program usage: Uses either COM1 or COM2	
+	const char * serialPortName = argv[1];
+		
+	if (argc < 2) {
+  		printf("Incorrect program usage\n"
+  		"Usage: %s <SerialPort>\n"
+    	"Example: %s /dev/ttyS1\n", argv[0], argv[0]);
+  	exit(1);
 
-        // Open serial port device for reading and writing, and not as controlling tty
-        // because we don't want to get killed if linenoise sends CTRL-C.
-        int fd = open(serialPortName, O_RDWR | O_NOCTTY);
+	// Open serial port device for reading and writing, and not as controlling tty
+	// because we don't want to get killed if linenoise sends CTRL-C.
+	int fd = open(serialPortName, O_RDWR | O_NOCTTY);
 
-        if (fd < 0)
-        {
-            perror(serialPortName);
-            exit(-1);
-        }
+	if (fd < 0) {
+  		perror(serialPortName);
+  		exit(-1);
+	}
 
-        struct termios oldtio;
-        struct termios newtio;
+	struct termios oldtio;
+	struct termios newtio;
 
-        // Save current port settings
-        if (tcgetattr(fd, &oldtio) == -1)
-        {
-            perror("tcgetattr");
-            exit(-1);
-        }
+	// Save current port settings
+	if (tcgetattr(fd, & oldtio) == -1) {
+  		perror("tcgetattr");
+  		exit(-1);
+	}
 
-        // Clear struct for new port settings
-        memset(&newtio, 0, sizeof(newtio));
+	// Clear struct for new port settings
+	memset( & newtio, 0, sizeof(newtio));
 
-        newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-        newtio.c_iflag = IGNPAR;
-        newtio.c_oflag = 0;
+	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+	newtio.c_iflag = IGNPAR;
+	newtio.c_oflag = 0;
 
-        // Set input mode (non-canonical, no echo,...)
-        newtio.c_lflag = 0;
-        newtio.c_cc[VTIME] = 0; // Inter-character timer unused
-        newtio.c_cc[VMIN] = 5;  // Blocking read until 5 chars received
+	// Set input mode (non-canonical, no echo,...)
+	newtio.c_lflag = 0;
+	newtio.c_cc[VTIME] = 0; // Inter-character timer unused
+	newtio.c_cc[VMIN] = 5; // Blocking read until 5 chars received
 
-        // VTIME e VMIN should be changed in order to protect with a
-        // timeout the reception of the following character(s)
+	// VTIME e VMIN should be changed in order to protect with a	
+	// timeout the reception of the following character(s)	
 
-        // Now clean the line and activate the settings for the port
-        // tcflush() discards data written to the object referred to
-        // by fd but not transmitted, or data received but not read,
-        // depending on the value of queue_selector:
-        //   TCIFLUSH - flushes data received but not read.
-        tcflush(fd, TCIOFLUSH);
+	// Now clean the line and activate the settings for the port
+	// tcflush() discards data written to the object referred to
+	// by fd but not transmitted, or data received but not read,
+	// depending on the value of queue_selector:
+	//   TCIFLUSH - flushes data received but not read.
+	tcflush(fd, TCIOFLUSH);
 
-        // Set new port settings
-        if (tcsetattr(fd, TCSANOW, &newtio) == -1)
-        {
-            perror("tcsetattr");
-            exit(-1);
-        }
+	// Set new port settings
+	if (tcsetattr(fd, TCSANOW, & newtio) == -1) {
+  		perror("tcsetattr");
+  		exit(-1);
+	}
 
-        printf("New termios structure set\n");
-        /*
+	printf("New termios structure set\n");
+	/*
         // Create string to send
          while (alarmCount < 4)
          {
@@ -207,16 +199,15 @@ int main(int argc, char *argv[])
             }
          }
          */
-     // Restore the old port settings
-     if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
-     {
-        perror("tcsetattr");
-        exit(-1);
-     }
+	// Restore the old port settings
+	if (tcsetattr(fd, TCSANOW, & oldtio) == -1) {
+	  perror("tcsetattr");
+	  exit(-1);
+	}
 
-    close(fd);
-    
-    printf("Ending program\n");
-   
-    return 0;
+	close(fd);
+
+	printf("Ending program\n");
+
+	return 0;
 }
